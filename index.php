0dae1407aa8996126f665829a312711fa6732bff
AlterSTI <alterstii@gmail.com>
2022-05-10 10:23:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,6 @@ require_once 'Api.php';
 require_once 'DataCache.php';
 require_once 'SiteSettings.php';
 
-function pre($array){
-    echo '<pre>';
-    print_r($array);
-    echo '</pre>';
-}
-
-function _pre($array){
-    echo '<pre>';
-    var_dump($array);
-    echo '</pre>';
-}
-
 try {
     $api = new Api();
     echo $api->run();
